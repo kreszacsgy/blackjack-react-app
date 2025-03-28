a664e99d2e59d984dd5f257c3e3ce426f0a9319f
kreszacsgy <kreszacsgy@gmail.com>
2025-03-28 19:21:41 +0100

    Status component

--- src/components/Status/Status.jsx
+++ src/components/Status/Status.jsx
@@ -1,4 +1,21 @@
-const Status=()=>{
+
+import React from 'react';
+import styles from './Status.css';
+
+
+
+const Status= (props) => {
+    const {message,balance}=props;
+  return (
+    <div className={styles.statusContainer}>
+      <div className={styles.status}>
+        <h1 className={styles.value}>{message}</h1>
+      </div>
+      <div className={styles.balance}>
+        <h1 className={styles.value}>${balance}</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Status;
