7017a07dfd3b1a7495cdf7fd0398953c5e9cd769
kreszacsgy <kreszacsgy@gmail.com>
2025-03-28 15:59:10 +0100

    components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Status from './components/Status/Status';
+import Controls from './components/Controls/Controls';
+import Hand from './components/Hand/Hand';
+
+function App() {
+  return (
+    <>
+      <Status/>
+      <Controls/>
+      <Hand/>
+      <Hand/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hand/Hand.jsx
@@ -0,0 +1,4 @@
+const Hand=()=>{
+}
+
+export default Hand;
