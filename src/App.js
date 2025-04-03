1bf97a4c2044edead90813bcbc3707c46225a8d7
kreszacsgy <kreszacsgy@gmail.com>
2025-04-03 14:11:59 +0200

    draw and render cards from API

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import './App.css';
 import Status from './components/Status/Status';
 import Controls from './components/Controls/Controls';
 import Hand from './components/Hand/Hand';
+import { useState } from 'react';
 
 function App() {
+  const [deckId ,setDeckId]=useState(null);
+
+  const NewGame=(event)=>{
+    event.preventDefault();
+    fetch ("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+      .then((response) => response.json())
+      .then((data) => {
+        
+        setDeckId(data.deck_id);
+      })
+  }
+ 
+
   return (
     <>
+      <h1>BlackJack</h1>
       <Status/>
       <Controls/>
-      <Hand/>
-      <Hand/>
+      <button onClick={NewGame}>New Game</button>
+      <Hand  title={"Dealer's Hand"} deckId={deckId}/>
+      <Hand title={"Your Hand"} deckId={deckId}/>
     </>
   );
 }
--- src/components/Hand/Hand.jsx
+++ src/components/Hand/Hand.jsx
@@ -1,4 +1,40 @@
-const Hand=()=>{
+import React, { useState } from 'react';
+import styles from './Hand.css';
+
+
+
+
+const Hand = ({title,deckId}) => {
+    const [cards ,setCards]=useState([]);
+
+    const drawCard=(event)=>{
+        event.preventDefault();
+        fetch (`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+            .then((response) => response.json())
+            .then((data) => {setCards(data.cards)}
+        )
+    }
+
+    const renderCards=()=>{
+        return cards.map(card=><img src={card.image} alt={card.code} key={card.code}/>)
+    }
+   
+    const getTitle = () => {
+        if (cards.length > 0) {
+            return (
+            <h1 className={styles.title}>{title}</h1>
+            );
+        }
+    }
+
+    return (
+        <div className={styles.handContainer}>
+            {getTitle()}
+            <div className={styles.cardContainer}>{renderCards()}</div>
+            <button onClick={drawCard}>Draw</button>
+        
+        </div>
+    );
 }
 
 export default Hand;
